a560f00a79b62feaa854ba5988c39cd589094c07
unknown <chetansharma777555@gmail.com>
2025-12-16 20:11:41 +0530

    db.ts updated

--- server/db.ts
+++ server/db.ts
@@ -2,12 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "@shared/schema";
 
-const DATABASE_URL = process.env.DATABASE_URL;
+// Force TypeScript to treat this as a string with "!"
+const sql = neon(process.env.DATABASE_URL!);
 
-if (!DATABASE_URL && process.env.NODE_ENV === "production") {
-  throw new Error("DATABASE_URL must be set");
-}
-
-
-const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql, { schema });
